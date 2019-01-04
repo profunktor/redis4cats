cb379b711cf9526a6a430ed5ed5942cd8f58bb47
Gabriel Volpe <volpegabriel@gmail.com>
2019-01-04 10:14:07 +0900

    Simplifying instance impl

--- core/src/test/scala/com/github/gvolpe/fs2redis/codecs/SplitMorphismTest.scala
+++ core/src/test/scala/com/github/gvolpe/fs2redis/codecs/SplitMorphismTest.scala
@@ -37,7 +37,7 @@ object TestSplitEpiInstances {
 
   // Just proving that these form a split monomorphism and won't pass the laws of epimorphisms
   val intDoubleMono: SplitMono[Int, Double] =
-    SplitMono(s => Try(s.toDouble).getOrElse(0), _.toInt)
+    SplitMono(_.toDouble, _.toInt)
 
   val intStringMono: SplitMono[Int, String] =
     SplitMono(_.toString, s => Try(s.toInt).getOrElse(0))
