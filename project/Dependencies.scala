c77a4a2654815a0faca6d31732f53d3e7a4cf6c6
Scala Steward <me@scala-steward.org>
2019-06-12 11:24:47 +0200

    Update lettuce-core to 5.1.7.RELEASE

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -6,7 +6,7 @@ object Dependencies {
     val cats       = "1.6.1"
     val catsEffect = "1.3.1"
     val fs2        = "1.0.4"
-    val lettuce    = "5.1.6.RELEASE"
+    val lettuce    = "5.1.7.RELEASE"
     val log4cats   = "0.3.0"
     val logback    = "1.2.3"
 
