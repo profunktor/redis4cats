48bf503cb55231ca3629c313cf3eb18d64dda4c7
Jakub Kozłowski <kubukoz@gmail.com>
2019-06-24 18:08:42 +0200

    Update Dependencies.scala

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -10,6 +10,7 @@ object Dependencies {
     val kindProjector    = "0.10.3"
 
     val scalaCheck = "1.14.0"
+    val scalaTest  = "3.0.8"
   }
 
   object Versions212 {
@@ -17,8 +18,6 @@ object Dependencies {
     val catsEffect = "1.3.1"
     val fs2        = "1.0.5"
     val log4cats   = "0.3.0"
-
-    val scalaTest  = "3.0.8"
   }
   
   object Versions213 {
@@ -26,36 +25,33 @@ object Dependencies {
     val catsEffect = "2.0.0-M4"
     val fs2        = "1.1.0-M1"
     val log4cats   = "0.4.0-M1"
-
-    val scalaTest  = "3.1.0-SNAP13"
   }
 
   object Libraries {
-    lazy val redisClient = "io.lettuce"    % "lettuce-core" % Versions.lettuce
+    val redisClient = "io.lettuce" % "lettuce-core" % Versions.lettuce
 
-    lazy val logback = "ch.qos.logback" % "logback-classic" % Versions.logback
+    val logback = "ch.qos.logback" % "logback-classic" % Versions.logback
 
     // Compiler plugins
-    lazy val betterMonadicFor = "com.olegpy"     %% "better-monadic-for" % Versions.betterMonadicFor
-    lazy val kindProjector    = "org.typelevel" %% "kind-projector"     % Versions.kindProjector
+    val betterMonadicFor = "com.olegpy"     %% "better-monadic-for" % Versions.betterMonadicFor
+    val kindProjector    = "org.typelevel" %% "kind-projector"      % Versions.kindProjector
 
     // Scala test libraries
-    lazy val scalaCheck  = "org.scalacheck" %% "scalacheck"   % Versions.scalaCheck
+    val scalaCheck  = "org.scalacheck" %% "scalacheck" % Versions.scalaCheck
+    val scalaTest   = "org.scalatest"  %% "scalatest"  % Versions.scalaTest
   }
 
   object Libraries213 {
     def log4cats(artifact: String): ModuleID = "io.chrisdavenport" %% s"log4cats-$artifact" % Versions213.log4cats
 
-    lazy val catsEffect  = "org.typelevel" %% "cats-effect" % Versions213.catsEffect
-    lazy val fs2Core     = "co.fs2"        %% "fs2-core"    % Versions213.fs2
+    val catsEffect  = "org.typelevel" %% "cats-effect" % Versions213.catsEffect
+    val fs2Core     = "co.fs2"        %% "fs2-core"    % Versions213.fs2
 
-    lazy val log4CatsCore  = log4cats("core")
-    lazy val log4CatsSlf4j = log4cats("slf4j")
+    val log4CatsCore  = log4cats("core")
+    val log4CatsSlf4j = log4cats("slf4j")
 
-    lazy val catsLaws    = "org.typelevel"  %% "cats-core"    % Versions213.cats
-    lazy val catsTestKit = "org.typelevel"  %% "cats-testkit" % Versions213.cats
-
-    lazy val scalaTest   = "org.scalatest"  %% "scalatest"    % Versions213.scalaTest
+    val catsLaws    = "org.typelevel"  %% "cats-core"    % Versions213.cats
+    val catsTestKit = "org.typelevel"  %% "cats-testkit" % Versions213.cats
   }
 
   object Libraries212 {
@@ -64,12 +60,10 @@ object Dependencies {
     lazy val catsEffect  = "org.typelevel" %% "cats-effect" % Versions212.catsEffect
     lazy val fs2Core     = "co.fs2"        %% "fs2-core"    % Versions212.fs2
 
-    lazy val log4CatsCore  = log4cats("core")
-    lazy val log4CatsSlf4j = log4cats("slf4j")
+    val log4CatsCore  = log4cats("core")
+    val log4CatsSlf4j = log4cats("slf4j")
 
-    lazy val catsLaws    = "org.typelevel"  %% "cats-core"    % Versions212.cats
-    lazy val catsTestKit = "org.typelevel"  %% "cats-testkit" % Versions212.cats
-
-    lazy val scalaTest   = "org.scalatest"  %% "scalatest"    % Versions212.scalaTest
+    val catsLaws    = "org.typelevel"  %% "cats-core"    % Versions212.cats
+    val catsTestKit = "org.typelevel"  %% "cats-testkit" % Versions212.cats
   }
 }
