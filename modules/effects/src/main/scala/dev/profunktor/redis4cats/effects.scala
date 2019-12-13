e0036b7bab164dad6b8ec43b727bc0111cbc4af4
Christopher Davenport <Chris@ChristopherDavenport.tech>
2019-12-12 16:02:44 -0800

    Make Final, Add Documentation to Args

--- modules/effects/src/main/scala/dev/profunktor/redis4cats/effects.scala
+++ modules/effects/src/main/scala/dev/profunktor/redis4cats/effects.scala
@@ -22,35 +22,39 @@ import scala.concurrent.duration.FiniteDuration
 
 object effects {
 
-  case class Distance(value: Double) extends AnyVal
-  case class GeoHash(value: Long) extends AnyVal
-  case class Latitude(value: Double) extends AnyVal
-  case class Longitude(value: Double) extends AnyVal
+  final case class Distance(value: Double) extends AnyVal
+  final case class GeoHash(value: Long) extends AnyVal
+  final case class Latitude(value: Double) extends AnyVal
+  final case class Longitude(value: Double) extends AnyVal
 
-  case class GeoLocation[V](lon: Longitude, lat: Latitude, value: V)
-  case class GeoRadius(lon: Longitude, lat: Latitude, dist: Distance)
+  final case class GeoLocation[V](lon: Longitude, lat: Latitude, value: V)
+  final case class GeoRadius(lon: Longitude, lat: Latitude, dist: Distance)
 
-  case class GeoCoordinate(x: Double, y: Double)
-  case class GeoRadiusResult[V](value: V, dist: Distance, hash: GeoHash, coordinate: GeoCoordinate)
-  case class GeoRadiusKeyStorage[K](key: K, count: Long, sort: GeoArgs.Sort)
-  case class GeoRadiusDistStorage[K](key: K, count: Long, sort: GeoArgs.Sort)
+  final case class GeoCoordinate(x: Double, y: Double)
+  final case class GeoRadiusResult[V](value: V, dist: Distance, hash: GeoHash, coordinate: GeoCoordinate)
+  final case class GeoRadiusKeyStorage[K](key: K, count: Long, sort: GeoArgs.Sort)
+  final case class GeoRadiusDistStorage[K](key: K, count: Long, sort: GeoArgs.Sort)
 
-  case class Score(value: Double) extends AnyVal
-  case class ScoreWithValue[V](score: Score, value: V)
-  case class ZRange[V](start: V, end: V)
-  case class RangeLimit(offset: Long, count: Long)
+  final case class Score(value: Double) extends AnyVal
+  final case class ScoreWithValue[V](score: Score, value: V)
+  final case class ZRange[V](start: V, end: V)
+  final case class RangeLimit(offset: Long, count: Long)
 
   sealed trait SetArg
   object SetArg {
     sealed trait Existence extends SetArg
     object Existence {
+      /** Only set key if it does not exist */
       case object Nx extends Existence
+      /** Only set key if it already exists */
       case object Xx extends Existence
     }
 
     sealed trait Ttl extends SetArg
     object Ttl {
+      /** Set Expiration in Millis */
       case class Px(duration: FiniteDuration) extends Ttl
+      /** Set Expiration in Seconds */
       case class Ex(duration: FiniteDuration) extends Ttl
     }
   }
