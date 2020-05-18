902c9b85329b2103007900bbd360c66c0f61c747
Gabriel Volpe <volpegabriel@gmail.com>
2020-05-18 18:08:49 +0200

    jdk + sbt + sha256

--- shell.nix
+++ shell.nix
@@ -1,9 +1,15 @@
 let
   # unstable packages on May 13th
-  pkgs = import (fetchTarball "https://github.com/NixOS/nixpkgs-channels/archive/6bcb1dec8ea.tar.gz") {};
+  nixpkgs = fetchTarball {
+    url = "https://github.com/NixOS/nixpkgs-channels/archive/6bcb1dec8ea.tar.gz";
+    sha256 = "04x750byjr397d3mfwkl09b2cz7z71fcykhvn8ypxrck8w7kdi1h";
+  };
+  pkgs = import nixpkgs {};
 in
   pkgs.mkShell {
     buildInputs = [
-      pkgs.jekyll # v4.0.1
+      pkgs.jekyll # 4.0.1
+      pkgs.openjdk # 1.8.0_242
+      pkgs.sbt # 1.3.10
     ];
   }
