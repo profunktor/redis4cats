cddaafd977f212b122c6480a524b9b1cc7e074ab
Gabriel Volpe <volpegabriel@gmail.com>
2020-05-31 20:31:57 +0200

    First try

--- shell.nix
+++ shell.nix
@@ -1,7 +1,7 @@
 let
-  # unstable packages on May 13th
   nixpkgs = fetchTarball {
-    url = "https://github.com/NixOS/nixpkgs-channels/archive/6bcb1dec8ea.tar.gz";
+    name   = "NixOS-unstable-13-05-2020";
+    url    = "https://github.com/NixOS/nixpkgs-channels/archive/6bcb1dec8ea.tar.gz";
     sha256 = "04x750byjr397d3mfwkl09b2cz7z71fcykhvn8ypxrck8w7kdi1h";
   };
   pkgs = import nixpkgs {};
