ef224434c1c58add35756baa17230006c7226dab
Scala Steward <me@scala-steward.org>
2021-02-10 18:48:18 +0100

    Update cats-core, cats-testkit to 2.4.1

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,8 +3,8 @@ import sbt._
 object Dependencies {
 
   object V {
-    val cats       = "2.3.1"
-    val catsEffect = "2.3.1"
+    val cats       = "2.4.1"
+    val catsEffect = "2.4.1"
     val circe      = "0.13.0"
     val fs2        = "2.5.0"
     val log4cats   = "1.1.1"
