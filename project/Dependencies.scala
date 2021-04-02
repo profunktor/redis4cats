567af6e3e286065b45bca4222085dd9d10a87599
Scala Steward <me@scala-steward.org>
2021-04-02 10:42:37 +0200

    Update log4cats-core, log4cats-slf4j to 2.0.1

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -7,7 +7,7 @@ object Dependencies {
     val catsEffect = "3.0.1"
     val circe      = "0.14.0-M4"
     val fs2        = "3.0.1"
-    val log4cats   = "2.0.0"
+    val log4cats   = "2.0.1"
 
     val lettuce = "6.1.0.RELEASE"
     val logback = "1.2.3"
