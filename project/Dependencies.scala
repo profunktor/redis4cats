e21e0f210315251a591cbf58c5d68d47aa3d5216
Scala Steward <me@scala-steward.org>
2021-07-01 18:56:28 +0200

    Update fs2-core to 3.0.5

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -6,7 +6,7 @@ object Dependencies {
     val cats       = "2.6.1"
     val catsEffect = "3.1.1"
     val circe      = "0.14.1"
-    val fs2        = "3.0.4"
+    val fs2        = "3.0.5"
     val log4cats   = "2.1.1"
 
     val lettuce = "6.1.3.RELEASE"
