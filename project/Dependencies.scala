4d4dd8e1241a909493df08e6e9fa841e28872fab
Scala Steward <me@scala-steward.org>
2021-07-14 14:48:33 +0200

    Update lettuce-core to 6.1.4.RELEASE

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -9,7 +9,7 @@ object Dependencies {
     val fs2        = "3.0.6"
     val log4cats   = "2.1.1"
 
-    val lettuce = "6.1.3.RELEASE"
+    val lettuce = "6.1.4.RELEASE"
     val logback = "1.2.3"
 
     val kindProjector = "0.13.0"
