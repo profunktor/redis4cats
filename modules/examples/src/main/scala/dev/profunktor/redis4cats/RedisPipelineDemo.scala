f17adc78d6304c4a489e9c59d407ea64924487e3
Gabriel Volpe <volpegabriel@gmail.com>
2021-08-28 11:43:11 +0200

    fix updates of cats-effect & fs2

--- modules/examples/src/main/scala/dev/profunktor/redis4cats/RedisPipelineDemo.scala
+++ modules/examples/src/main/scala/dev/profunktor/redis4cats/RedisPipelineDemo.scala
@@ -17,7 +17,6 @@
 package dev.profunktor.redis4cats
 
 import cats.effect._
-import cats.syntax.all._
 import dev.profunktor.redis4cats.effect.Log.NoOp._
 import dev.profunktor.redis4cats.hlist._
 import dev.profunktor.redis4cats.pipeline._
