9c8cefd73aa95a6db45c54660a3fe99ae6fa66d9
Scala Steward <me@scala-steward.org>
2021-09-04 06:47:18 +0200

    Update cats-effect, cats-effect-kernel to 3.2.7

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -4,7 +4,7 @@ object Dependencies {
 
   object V {
     val cats       = "2.6.1"
-    val catsEffect = "3.2.5"
+    val catsEffect = "3.2.7"
     val circe      = "0.14.1"
     val fs2        = "3.1.1"
     val log4cats   = "2.1.1"
