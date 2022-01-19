bce902ca692f98322240ae4a3822dcd5c0b43cf9
Scala Steward <me@scala-steward.org>
2022-01-19 02:36:38 +0100

    Applied Scalafix rule(s) github:typelevel/cats/Cats_v2_2_0?sha=v2.2.0
    See https://github.com/typelevel/cats/blob/v2.2.0/scalafix/README.md#migration-to-cats-v220 for details

--- modules/examples/src/main/scala/dev/profunktor/redis4cats/RedisBitmapsDemo.scala
+++ modules/examples/src/main/scala/dev/profunktor/redis4cats/RedisBitmapsDemo.scala
@@ -17,7 +17,7 @@
 package dev.profunktor.redis4cats
 
 import cats.effect.{ IO, Resource }
-import cats.implicits._
+import cats.syntax.all._
 import dev.profunktor.redis4cats.algebra.BitCommandOperation.{ IncrUnsignedBy, SetUnsigned }
 import dev.profunktor.redis4cats.algebra.BitCommands
 import dev.profunktor.redis4cats.effect.Log.NoOp._
