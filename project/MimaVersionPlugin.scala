7b6074dc8733fa4e885029c277a291836f295753
yunion <ysrlunion@gmail.com>
2024-06-06 13:05:47 -0400

    put back all check snapshot true

--- project/MimaVersionPlugin.scala
+++ project/MimaVersionPlugin.scala
@@ -42,7 +42,7 @@ object MimaVersionPlugin extends AutoPlugin {
       )
     }
   }
-/*
+
   override def buildSettings: Seq[Setting[_]] =
     GitPlugin.autoImport.versionWithGit ++ Seq(
           git.gitTagToVersionNumber := {
@@ -73,7 +73,7 @@ object MimaVersionPlugin extends AutoPlugin {
           git.gitCurrentTags := Try(
                 "git tag --contains HEAD".!!.trim.split("\\s+").toList.filter(_ != "")
               ).toOption.toList.flatten
-        )*/
+        )
 
   override def projectSettings: Seq[Setting[_]] = Seq(
     isMimaEnabled := false,
