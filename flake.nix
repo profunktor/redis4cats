825a222bcfd3184e541fc1cd20ad4046200b2b49
Gabriel Volpe <volpegabriel@gmail.com>
2025-04-26 15:34:19 +0200

    nix: use dev-tools

--- flake.nix
+++ flake.nix
@@ -1,48 +1,9 @@
 {
-  description = "Scala development shell";
+  description = "ProfunKtor - Scala development tools";
 
-  inputs = {
-    nixpkgs.url = "nixpkgs/nixos-unstable";
-    flake-utils.url = github:numtide/flake-utils;
+  inputs.dev-tools.url = github:profunktor/dev-tools;
+
+  outputs = { dev-tools, ... }: {
+    inherit (dev-tools) devShells packages;
   };
-
-  outputs = { nixpkgs, flake-utils, ... }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        overlays = [
-          (self: super: {
-            jre = super.jdk21_headless;
-            sbt = super.sbt.overrideAttrs (
-              old: {
-                nativeBuildInputs = old.nativeBuildInputs or [ ] ++ [ super.makeWrapper ];
-                # Setting SBT_OPTS because of this bug: https://github.com/sbt/sbt-site/issues/169
-                postInstall = ''
-                  wrapProgram $out/bin/sbt --suffix SBT_OPTS : '--add-opens java.base/java.lang=ALL-UNNAMED'
-                '';
-              }
-            );
-          })
-        ];
-        pkgs = import nixpkgs { inherit system overlays; };
-      in
-      {
-        devShell = pkgs.mkShell {
-          name = "scala-dev-shell";
-
-          buildInputs = with pkgs; [
-            gnupg
-            jekyll
-            jre
-            sbt
-          ];
-
-          shellHook = ''
-            JAVA_HOME="${pkgs.jre}"
-          '';
-        };
-
-        packages = {
-          inherit (pkgs) sbt;
-        };
-      });
 }
