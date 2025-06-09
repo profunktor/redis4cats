aa14681fe28162063a8967a85a8b84ddf5e52c39
Yisrael Union <ysrlunion@gmail.com>
2025-06-09 13:43:04 -0400

    update deps (#1033)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -7,10 +7,10 @@ object Dependencies {
     val catsEffect = "3.6.1"
     val circe      = "0.14.13"
     val fs2        = "3.12.0"
-    val log4cats   = "2.7.0"
+    val log4cats   = "2.7.1"
     val keyPool    = "0.4.10"
 
-    val lettuce = "6.6.0.RELEASE"
+    val lettuce = "6.7.1.RELEASE"
 
     val logback = "1.5.18"
 
