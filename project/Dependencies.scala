c93fa738599e2d96673df154934465d505d10128
Gabriel Volpe <volpegabriel@gmail.com>
2025-07-14 05:38:31 +0200

    Update cats-effect, cats-effect-kernel to 3.6.2 (#1043)
    Co-authored-by: gvolpe-steward[bot] <gvolpe-steward[bot]@users.noreply.github.com>

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -4,7 +4,7 @@ object Dependencies {
 
   object V {
     val cats       = "2.13.0"
-    val catsEffect = "3.6.1"
+    val catsEffect = "3.6.2"
     val circe      = "0.14.14"
     val fs2        = "3.12.0"
     val log4cats   = "2.7.1"
