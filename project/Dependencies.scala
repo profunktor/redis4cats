ed99c38bc5c98efbc50a676f9507b5364cf25a13
Gabriel Volpe <volpegabriel@gmail.com>
2025-09-08 05:43:11 +0200

    Update fs2-core to 3.12.2 (#1051)
    Co-authored-by: gvolpe-steward[bot] <gvolpe-steward[bot]@users.noreply.github.com>

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -6,7 +6,7 @@ object Dependencies {
     val cats       = "2.13.0"
     val catsEffect = "3.6.3"
     val circe      = "0.14.14"
-    val fs2        = "3.12.0"
+    val fs2        = "3.12.2"
     val log4cats   = "2.7.1"
     val keyPool    = "0.4.10"
 
