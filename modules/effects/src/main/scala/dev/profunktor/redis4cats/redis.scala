f0b3d54be91f149737f6075771131fb87b83e965
YisraelU <ysrlunion@gmail.com>
2025-12-30 10:51:50 -0500

    fix json algebra

--- modules/effects/src/main/scala/dev/profunktor/redis4cats/redis.scala
+++ modules/effects/src/main/scala/dev/profunktor/redis4cats/redis.scala
@@ -792,25 +792,9 @@ private[redis4cats] class BaseRedis[F[_]: FutureLift: MonadThrow: Log, K, V](
     async.flatMap(_.jsonGet(key, options, all: _*).futureLift.map(_.asScala.toList))
   }
 
-  override def jGetRaw(key: K, path: JsonPath, paths: JsonPath*): F[List[String]] = {
-    val all = path +: paths
-    async.flatMap(_.jsonGetRaw(key, all: _*).futureLift.map(_.asScala.toList))
-  }
-
-  override def jGetRaw(key: K, arg: json.JsonGetArgs, path: JsonPath, paths: JsonPath*): F[List[String]] = {
-    val all     = path +: paths
-    val options = arg.underlying
-    async.flatMap(_.jsonGetRaw(key, options, all: _*).futureLift.map(_.asScala.toList))
-  }
-
   override def jMget(path: JsonPath, key: K, keys: K*): F[List[JsonValue]] = {
     val all = key +: keys
     async.flatMap(_.jsonMGet(path, all: _*).futureLift.map(_.asScala.toList))
-  }
-
-  override def jMgetRaw(path: JsonPath, key: K, keys: K*): F[List[String]] = {
-    val all = key +: keys
-    async.flatMap(_.jsonMGetRaw(path, all: _*).futureLift.map(_.asScala.toList))
   }
 
   override def jObjKeys(key: K, path: JsonPath): F[List[V]] =
@@ -867,15 +851,6 @@ private[redis4cats] class BaseRedis[F[_]: FutureLift: MonadThrow: Log, K, V](
 
   override def arrPop(key: K): F[List[JsonValue]] =
     async.flatMap(_.jsonArrpop(key).futureLift.map(_.asScala.toList))
-
-  override def arrPopRaw(key: K, path: JsonPath, index: Int): F[List[String]] =
-    async.flatMap(_.jsonArrpopRaw(key, path, index).futureLift.map(_.asScala.toList))
-
-  override def arrPopRaw(key: K, path: JsonPath): F[List[String]] =
-    async.flatMap(_.jsonArrpopRaw(key, path).futureLift.map(_.asScala.toList))
-
-  override def arrPopRaw(key: K): F[List[String]] =
-    async.flatMap(_.jsonArrpopRaw(key).futureLift.map(_.asScala.toList))
 
   override def arrTrim(key: K, path: JsonPath, range: JsonRangeArgs): F[List[Long]] =
     async.flatMap(_.jsonArrtrim(key, path, range).futureLift.map(_.asScala.toList.map(Long.unbox(_))))
