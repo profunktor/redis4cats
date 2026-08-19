741343f81ce1ecf4d50f6a74db83cbad0d3939ed
MarmaladeSky <alexey.kislin@gmail.com>
2026-08-19 15:15:52 -0700

    Put enableAllAdaptiveRefreshTriggers back (#1166)
    Due to lettuce upgrade being rollbacked
    
    Co-authored-by: David Akermann <david@junkie.digital>

--- modules/core/src/main/scala/dev/profunktor/redis4cats/connection/RedisClusterClient.scala
+++ modules/core/src/main/scala/dev/profunktor/redis4cats/connection/RedisClusterClient.scala
@@ -105,6 +105,7 @@ object RedisClusterClient {
               .topologyRefreshOptions(
                 ClusterTopologyRefreshOptions
                   .builder()
+                  .enableAllAdaptiveRefreshTriggers()
                   // Use implicit duration converters from scala 2.13 once 2.12 support is removed
                   .adaptiveRefreshTriggersTimeout(Duration.ofMillis(timeout.toMillis))
                   .build()
@@ -123,6 +124,7 @@ object RedisClusterClient {
                 ClusterTopologyRefreshOptions
                   .builder()
                   .enablePeriodicRefresh(Duration.ofMillis(interval.toMillis))
+                  .enableAllAdaptiveRefreshTriggers()
                   .adaptiveRefreshTriggersTimeout(Duration.ofMillis(timeout.toMillis))
                   .build()
               )
